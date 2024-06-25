a1bddb72d9d0ab228c668685315afb23409e56fb
unknown <assiatourabi55@gmail.com>
2024-06-25 23:12:25 +0100

    adding envirement

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//import { environment } from '../environments/environment';
+import { environment } from '../environments/environment';
 import { Employee } from './models/Employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiUrl = 'http://localhost:5268/api/employees';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5268/api/employees'
+};
+
